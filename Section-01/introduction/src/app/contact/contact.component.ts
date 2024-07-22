5486b66e5b39599cd9e50365a91e6841ea6153aa
Enes YALÇIN <107759141+eenesyalcin@users.noreply.github.com>
2024-07-22 10:16:53 +0300

    Section 1 Codes
    Application codes for the "Introduction" section have been uploaded.

---
+++ Section-01/introduction/src/app/contact/contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-contact",
+    standalone: true,
+    template: "<p style='color: green'>Contact Page</p>"
+})
+export class ContactComponent{
+    
+}
