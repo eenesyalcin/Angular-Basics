b0cb8bceb9f634245daddc75a2e3877e5e3d03fe
Enes YALÇIN <107759141+eenesyalcin@users.noreply.github.com>
2024-07-23 17:09:47 +0300

    Section 4 Codes
    Application codes for the "Routing" section have been uploaded.

---
+++ Section-04/routing/src/app/app.routes.ts
@@ -0,0 +1,42 @@
+import { Route, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+
+export const homeRoute: Route = {
+    path: "",
+    component: HomeComponent
+}
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "",
+        component: LayoutComponent,
+        children: [
+            homeRoute,
+            {
+                path: "home",
+                component: HomeComponent
+            },
+            {
+                path: "about",
+                component: AboutComponent
+            },
+            {
+                path: "contact",
+                component: ContactComponent
+            },
+            {
+                path: "contact/:params",
+                component: ContactComponent
+            }
+        ]
+    },
+    
+];
