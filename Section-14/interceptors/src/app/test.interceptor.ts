682008695fdce1411617ab8ef49a31c14a34faf3
Enes YALÇIN <107759141+eenesyalcin@users.noreply.github.com>
2024-07-29 17:03:46 +0300

    Section 14 Codes
    Application codes for the "Interceptors" section have been uploaded.

---
+++ Section-14/interceptors/src/app/test.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, of } from 'rxjs';
+
+export const testInterceptor: HttpInterceptorFn = (req, next) => {
+  let token = localStorage.getItem("token");
+  let newRequest = req.clone({
+    headers: req.headers.set("Authentication", "Bearer " + token)
+  });
+  return next(newRequest).pipe(
+    catchError((err: HttpErrorResponse) => {
+      // ERROR HANDLER
+      console.log(err);
+
+      return of()
+    })
+  );
+};
